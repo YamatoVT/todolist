816864254779cb8a7667354fc37a13d6c28d8b01
Yamato <gabrielj2814@hotmail.com>
2022-06-02 21:07:48 -0400

    agregando aquetado de la modal formulario y funcion de gurdar funcional

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// clases  
+var Tarea = /** @class */ (function () {
+    function Tarea(nombreTarea_, fechaCreacionTarea_, estadoTarea_) {
+        this.nombreTarea = nombreTarea_;
+        this.fechaCreacionTarea = fechaCreacionTarea_;
+        this.estadoTarea = estadoTarea_;
+    }
+    Tarea.prototype.setColorTarea = function (color_) {
+        this.colorTarea = color_;
+    };
+    Tarea.prototype.setFechaTarea = function (fecha_inicio, fecha_cierre) {
+        this.fechaInicio = fecha_inicio;
+        this.fechaCierre = fecha_cierre;
+    };
+    Tarea.prototype.setDescripcionTarea = function (descipcion) {
+        this.descripcionTarea = descipcion;
+    };
+    Tarea.prototype.getNombreTarea = function () {
+        return this.nombreTarea;
+    };
+    return Tarea;
+}());
+// funciones franmari calle 6
+var listaDeTarea = [];
+function evitarEnviarDatos(a) {
+    a.preventDefault();
+}
+function guardar(lista_) {
+    var lista = lista_;
+    return function (dato) {
+        lista.push(dato);
+        return lista;
+    };
+}
+var guardarTarea = guardar(listaDeTarea);
+function capturardatosFormulario() {
+    var formulario = document.getElementById("fromularioTarea");
+    var datosFormulario = new FormData(formulario);
+    var nombreTarea = datosFormulario.get("nombreTarea");
+    var fechaCreacionTarea = datosFormulario.get("fechaCreacionTarea");
+    var estadoTarea = datosFormulario.get("estadoTarea");
+    var fechaInicio = datosFormulario.get("fechaInicio");
+    var fecheCierre = datosFormulario.get("fecheCierre");
+    var descripcionTarea = datosFormulario.get("descripcionTarea");
+    var tarea = new Tarea(nombreTarea, fechaCreacionTarea, estadoTarea);
+    if (fechaInicio && fecheCierre)
+        tarea.setFechaTarea(fechaInicio, fecheCierre);
+    if (descripcionTarea)
+        tarea.setDescripcionTarea(descripcionTarea);
+    console.log(guardarTarea(tarea));
+}
---
+++ js/index.ts
@@ -0,0 +1,84 @@
+// tipos
+interface TareaInterface{
+    nombreTarea:string,
+    fechaCreacionTarea:string
+    fechaInicio?:string,
+    fecheCierre?:string,
+    estadoTarea:string,
+    descripcionTarea?:string,
+    colorTarea?:string
+}
+
+
+
+
+
+// clases  
+class Tarea implements TareaInterface {
+
+    nombreTarea:string
+    fechaCreacionTarea:string
+    fechaCierre:string
+    fechaInicio:string
+    estadoTarea:string
+    descripcionTarea:string
+    colorTarea:string
+
+
+    constructor(nombreTarea_:string,fechaCreacionTarea_:string,estadoTarea_:string){
+        this.nombreTarea=nombreTarea_
+        this.fechaCreacionTarea=fechaCreacionTarea_
+        this.estadoTarea=estadoTarea_
+    }
+
+    setColorTarea(color_:string):void{
+        this.colorTarea=color_
+    }
+
+    setFechaTarea(fecha_inicio:string,fecha_cierre:string):void{
+        this.fechaInicio=fecha_inicio
+        this.fechaCierre=fecha_cierre
+    }
+
+    setDescripcionTarea(descipcion:string):void{
+        this.descripcionTarea=descipcion
+    }
+
+    getNombreTarea():string {
+        return this.nombreTarea
+    }
+
+}
+
+// funciones franmari calle 6
+let listaDeTarea:Array<Tarea>=[]
+
+function evitarEnviarDatos(a:Event):void{
+    a.preventDefault()
+}
+
+function guardar<T>(lista_:T[]):Function{
+    let lista:T[]=lista_
+    return (dato): any=> {
+        lista.push(dato)
+        return lista
+    }
+}
+
+let guardarTarea:Function=guardar(listaDeTarea)
+
+function capturardatosFormulario():void{
+    let formulario:HTMLFormElement=document.getElementById("fromularioTarea") as HTMLFormElement
+    let datosFormulario:FormData=new FormData(formulario)
+    let nombreTarea:string=datosFormulario.get("nombreTarea") as string
+    let fechaCreacionTarea:string=datosFormulario.get("fechaCreacionTarea") as string
+    let estadoTarea:string=datosFormulario.get("estadoTarea") as string
+    let fechaInicio:string=datosFormulario.get("fechaInicio") as string
+    let fecheCierre:string=datosFormulario.get("fecheCierre") as string
+    let descripcionTarea:string=datosFormulario.get("descripcionTarea") as string
+    let tarea:Tarea=new Tarea(nombreTarea,fechaCreacionTarea,estadoTarea)
+    if(fechaInicio && fecheCierre) tarea.setFechaTarea(fechaInicio,fecheCierre)
+    if(descripcionTarea) tarea.setDescripcionTarea(descripcionTarea)
+    console.log(guardarTarea(tarea))
+}
+
